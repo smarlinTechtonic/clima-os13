a6ae8af3512679f747a6b6febbad3f4bbcb29997
Sonali Marlin <sonali.marlin@techtonic.com>
2019-11-19 09:31:16 -0700

    Commit all functionality

---
+++ WeatherData.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Sonali Marlin on 11/18/19.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let main: Main
+    let weather: [Weather]
+}
+
+struct Main: Decodable {
+    let temp: Double
+}
+
+struct Weather: Decodable {
+    let description: String
+    let id: Int
+}
